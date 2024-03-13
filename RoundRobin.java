5472814b8a44514a445fce33914b12688a3a753e
Camilo Tejada <57298465+tejcamilo@users.noreply.github.com>
2024-03-13 18:05:19 -0500

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -9,54 +9,45 @@ package ur_os;
  * @author prestamour
  */
 public class RoundRobin extends Scheduler{
-
     int q;
     int cont;
-    
-    RoundRobin(OS os){
+    RoundRobin(OS os) {
         super(os);
         q = 5;
-        cont=0;
+        cont = 0;
     }
-    
-    RoundRobin(OS os, int q){
+    RoundRobin(OS os, int q) {
         this(os);
         this.q = q;
     }
-    
-   
+    void resetCounter(){
+        cont = 0;
+    }
+
     @Override
     public void getNext(boolean cpuEmpty) {
-        if(!processes.isEmpty() && cpuEmpty){
-            Process nextProcess = processes.poll();
-            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, nextProcess);
-            
-            int burst_time = nextProcess.getRemainingTimeInCurrentBurst();
-            if(burst_time <= q){
-                nextProcess.setRemainingTimeInCurrentBurst(0);
-                
-            } else{
-                nextProcess.setRemainingTimeInCurrentBurst(burst_time);
-                processes.offer(nextProcess);
+        // 4 casos
+        // 1. CPU vacio y hay procesos en la cola
+        // 2. CPU vacio y no hay procesos en la cola
+        // 3. CPU ocupado y hay procesos en la cola
+        // 4. CPU ocupado y no hay procesos en la cola
+        if (!cpuEmpty) { //cpu ocupada, verificar si cola vacia o no
+            cont++;
+            if(cont == q && !processes.isEmpty()){ // cola vacía
+                os.interrupt(InterruptType.SCHEDULER_CPU_TO_RQ, processes.poll());
+                cont = 0;
             }
-            
+        } else if(!processes.isEmpty()){ // cpu vacía, verificar queue
+            os.interrupt(InterruptType.SCHEDULER_RQ_TO_CPU, processes.poll());
+            cont=0;
         }
-        
-        
-        
     }
-    
+
+
     @Override
     public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
 
     @Override
     public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
-}
 
-/*
-Processs method:     
-public void setRemainingTimeInCurrentBurst(int new_time){
-        this.new_time = pbl.getRemainingTimeInCurrentBurst();
-    }
-*/
+}
