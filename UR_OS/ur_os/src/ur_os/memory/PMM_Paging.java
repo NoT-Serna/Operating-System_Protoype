307138554f8b2b36e6c5884bbfec098d1b94b974
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2024-05-01 17:24:58 -0500

    Update PMM_Paging.java

--- UR_OS/ur_os/src/ur_os/memory/PMM_Paging.java
+++ UR_OS/ur_os/src/ur_os/memory/PMM_Paging.java
@@ -2,8 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package ur_os.memory;
 
-package ur_os;
 import ur_os.process.ProcessMemoryManager;
 import ur_os.process.ProcessMemoryManagerType;
 
@@ -11,55 +11,59 @@ import ur_os.process.ProcessMemoryManagerType;
  *
  * @author super
  */
-public class PMM_Paging extends ProcessMemoryManagement {
+public class PMM_Paging extends ProcessMemoryManager{
+    
     PageTable pt;
 
     public PMM_Paging(int processSize) {
-        super(ProcessMemoryManagementType.PAGING, processSize);
+        super(ProcessMemoryManagerType.PAGING,processSize);
         pt = new PageTable(processSize);
     }
 
     public PMM_Paging(PageTable pt) {
         this.pt = pt;
     }
+    
+    public PMM_Paging(PMM_Paging pmm) {
+        super(pmm);
+        if(pmm.getType() == this.getType()){
+            this.pt = new PageTable(pmm.getPt());
+        }else{
+            System.out.println("Error - Wrong PMM parameter");
+        }
+    }
 
     public PageTable getPt() {
         return pt;
     }
-
-    public MemoryAddress getPageMemoryAddressFromLocalAddress(int locAdd) {
-        // Calculate page number and offset
-        int pageNumber = locAdd / PageTable.getPageSize();
+    
+    public void addFrameID(int frame){
+        pt.addFrameID(frame);
+    }
+    
+    public MemoryAddress getPageMemoryAddressFromLocalAddress(int locAdd){
+        int page_num = locAdd / PageTable.getPageSize();
         int offset = locAdd % PageTable.getPageSize();
-        return new MemoryAddress(pageNumber, offset);
+        return new MemoryAddress(page_num, offset);
     }
-
-    public MemoryAddress getFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m) {
-        // Find the frame number based on the memory address
-        int frameNumber = pt.getFrameIdFromPage(m.getPage_frame());
-        if (frameNumber == -1) {
-            // Page fault, frame not found
-            return null;
-        }
+    
+    public MemoryAddress getFrameMemoryAddressFromLogicalMemoryAddress(MemoryAddress m){
+        int frame_id = pt.getFrameIdFromPage(m.getPage_frame());
         int offset = m.getOffset();
-        return new MemoryAddress(frameNumber, offset);
+        return new MemoryAddress(frame_id,offset);
     }
-
-    public int getPhysicalAddress(int logicalAddress) {
-        MemoryAddress logicalMemoryAddress = getPageMemoryAddressFromLocalAddress(logicalAddress);
-        MemoryAddress frameMemoryAddress = getFrameMemoryAddressFromLogicalMemoryAddress(logicalMemoryAddress);
-        if (frameMemoryAddress == null) {
-            // Page fault, frame not found
-            return -1;
-        }
-        int frameNumber = frameMemoryAddress.getPage_frame();
-        int offset = frameMemoryAddress.getOffset();
-        // Calculate physical address
-        return frameNumber * PageTable.getPageSize() + offset;
+    
+   @Override
+    public int getPhysicalAddress(int logicalAddress){
+        
+        MemoryAddress page_address = getPageMemoryAddressFromLocalAddress(logicalAddress);
+        MemoryAddress frame_address = getFrameMemoryAddressFromLogicalMemoryAddress(page_address);
+        return (frame_address.getPage_frame() * PageTable.getPageSize()) + frame_address.getOffset();
     }
-
-    // Method to load a page into a specific frame
-    public void loadPageToFrame(int pageNumber, int frameNumber) {
-        pt.setFrameId(pageNumber, frameNumber);
+    
+     @Override
+    public String toString(){
+        return pt.toString();
     }
+    
 }
