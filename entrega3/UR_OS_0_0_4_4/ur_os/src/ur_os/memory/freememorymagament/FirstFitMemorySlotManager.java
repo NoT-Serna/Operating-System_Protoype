3bb971d05ba28259883c0a5bdc1f430acdddb97e
tejcamilo <ctejada@twilio.com>
2024-05-09 12:42:38 -0500

    Implemented FirstFit

---
+++ entrega3/UR_OS_0_0_4_4/ur_os/src/ur_os/memory/freememorymagament/FirstFitMemorySlotManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os.memory.freememorymagament;
+
+import ur_os.process.Process;
+
+/**
+ *
+ * @author super
+ */
+public class FirstFitMemorySlotManager extends FreeMemorySlotManager{
+
+    @Override
+    public MemorySlot getSlot(int size) {
+        MemorySlot m = null;
+        //To do
+        for(MemorySlot slot : list){
+            if(slot.getSize() >= size){
+                slot.setSize(slot.getSize()-size);
+                m = new MemorySlot(slot.getBase()+slot.getSize(),size);
+                break;
+            }
+        }
+        return m;
+    }
+
+    
+    
+}
