2044b2d4980a1d1a5fe8e0bf67999d75c6ddc888
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2024-05-20 20:04:37 -0500

    Add files via upload

---
+++ UR_OS_0_0_4_4_1/UR_OS_0_0_4_4_1/ur_os/src/ur_os/memory/freememorymagament/BestFitMemorySlotManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os.memory.freememorymagament;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author super
+ */
+public class BestFitMemorySlotManager extends FreeMemorySlotManager{
+    
+    @Override
+    public MemorySlot getSlot(int size) {
+        ArrayList<Integer> sizes = new ArrayList<>();
+        for(MemorySlot slot : list){
+            sizes.add(slot.getSize()-size);
+        }
+        int minIndex = 0;
+        for (int i = 1; i < sizes.size(); i++) {
+            if (sizes.get(i) >= 0 && sizes.get(i) < sizes.get(minIndex)) {
+                minIndex = i;
+            }
+        }
+        list.get(minIndex).setSize(list.get(minIndex).getSize()-size);
+        return new MemorySlot(list.get(minIndex).getBase()+list.get(minIndex).getSize(),size);
+    }
+}
